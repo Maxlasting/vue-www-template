55a361f74fc01571f83c1d90320679eb59656b2b
MaxLasting <best.fq.master@gmail.com>
2018-09-05 17:06:11 +0800

    2018-09-05-first commit

---
+++ config.js
@@ -0,0 +1,22 @@
+const { join } = require('path')
+
+module.exports = {
+  host: '0.0.0.0',
+  port: 8081,
+  // 代理服务配置，详见 https://webpack.js.org/configuration/dev-server/#devserver-proxy
+  proxy: {
+    // '/LiveExam': {
+    //   target: 'http://live.xueersi.com/science/',
+    //   changeOrigin: true
+    // }
+  },
+  // 静态资源存放目录，例如 mock 数据，如初始目录的 data.json 可以通过请求 /mock/data.json 得到
+  contentBase: join(__dirname, './static'),
+  // 对应 output 的 publicPath
+  publicPath: '/,
+  // 是否自动打开浏览器
+  open: false,
+  hot: true,
+  // 开发阶段，每次编译结束是否提示
+  alwaysNotify: false
+}
