c69c40d88bff17ad29814a7242a06ac0ac1869d4
MaxLasting <best.fq.master@gmail.com>
2018-09-05 17:43:40 +0800

    2018-09-05-ci03-fixed template

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
+<style scoped>
   .link {
     margin: 20px;
   }
--- src/views/Index/Index.vue
+++ src/views/Index/Index.vue
@@ -15,14 +15,11 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style>
   .root span {
     font-size: 30px;
     font-weight: bold;
     color: rgb(245, 45, 144);
     cursor: pointer;
-    &:hover {
-      color: rgb(221, 70, 39);
-    }
   }
 </style>
