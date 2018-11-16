ce77721cbd69ae05bacd931a93ddcf288addec11
MaxLasting <best.fq.master@gmail.com>
2018-11-16 16:18:10 +0800

    2018-11-16-ci02-1.0.2

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,8 @@
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+export default {}
 </script>
 
 <style>
-html,
-body {
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  height: 100%;
-}
 </style>
